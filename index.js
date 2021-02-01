ec7ea8493cf303f3cf21e23f28a5706b4426e09b
Josh Merlino <joshua.merlino@gmail.com>
2021-01-31 21:14:59 -0500

    update to ESModule syntax

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-module.exports = {
-	lookupName: require("./lib/lookup-name"),
-	lookupUUID: require("./lib/lookup-uuid")
-}
+import lookupName from "./lib/lookupName.js";
+import lookupUUID from "./lib/lookupUUID.js";
+export default { lookupName, lookupUUID };
