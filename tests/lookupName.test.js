95ca79707caf3eee2b81aefb0b976e4135504000
Josh Merlino <joshua.merlino@gmail.com>
2021-03-17 11:56:21 -0400

    add testing suite and eslint

---
+++ tests/lookupName.test.js
@@ -0,0 +1,10 @@
+/* eslint @typescript-eslint/no-var-requires: 0 */
+const { lookupName } = require("..");
+
+/* eslint no-undef: 0 */
+test("Test the 'lookupName' function.", async function() {
+	const NAME = "TehPicix";
+	const users = await lookupName(NAME);
+	const user = users.filter(user => user.currentName === NAME)[0];
+	expect(user.currentName).toBe(NAME);
+});
---
+++ tests/lookupUUID.test.js
@@ -0,0 +1,9 @@
+/* eslint @typescript-eslint/no-var-requires: 0 */
+const { lookupUUID } = require("..");
+
+/* eslint no-undef: 0 */
+test("Test the 'lookupUUID' function.", async function() {
+	const UUID = "1eb084b8-588e-43e6-bdd3-e05e53682987";
+	const user = await lookupUUID(UUID);
+	expect(user.uuid).toBe(UUID);
+});
