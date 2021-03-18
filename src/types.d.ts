0be3137f6526f050bf15d883382e9814e47f0375
Josh Merlino <joshua.merlino@gmail.com>
2021-03-18 17:05:55 -0400

    clean up tabs/spaces

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,14 @@
 export interface PastName {
-	name: string,
-	changedAt: null | number
+    name: string,
+    changedAt: null | number
 }
 
 export enum SkinModel { "classic", "slim" }
 
 export interface PastSkin {
-	url: string,
-	model: SkinModel
-	changedAt: null | number
+    url: string,
+    model: SkinModel
+    changedAt: null | number
 }
 
 export interface NameMCUser {
@@ -16,13 +16,13 @@ export interface NameMCUser {
     currentName: string,
     uuid: string,
     skins: {
-		currentSkin: string,
-		renders: {
-			cape: string,
-			body: string,
-			head: string,
-			face: string
-		}
+        currentSkin: string,
+        renders: {
+            cape: string,
+            body: string,
+            head: string,
+            face: string
+        }
         pastSkins: Array<PastSkin>
     },
     pastNames: Array<PastName>
