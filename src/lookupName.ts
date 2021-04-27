b570b1fc1126944cdf2ac90cfb0438e3b4801ae8
Josh Merlino <31785395+JoshMerlino@users.noreply.github.com>
2021-04-27 13:25:19 -0400

    initial commit

--- src/lookupName.ts
+++ src/lookupName.ts
@@ -1,42 +1,45 @@
 import { NameMCUser } from "./interface";
 import { lookupUUID as fetch } from "./lookupUUID";
-import { fetchDOM } from "./fetchDOM";
+import { fetchDOM } from "./fetchDom";
 
 /**
  *  @param {string} player
  */
 export async function lookupName(player: string): Promise<Array<NameMCUser>> {
+  // Make sure the username is a valid MC username
+  if (!player.match(/\w{3,16}/g))
+    throw new Error(`'${player}' is not a valid username.`);
 
-	// Make sure the username is a valid MC username
-	if (!player.match(/\w{3,16}/g)) throw new Error(`'${player}' is not a valid username.`);
+  // Fetch the DOM
+  const $ = await fetchDOM(`search?q=${player}`);
 
-	// Fetch the DOM
-	const $ = await fetchDOM(`search?q=${player}`);
+  // Get results
+  const results = $("body > main > div.row > div.col-lg-7").children(
+    ".card.mb-3"
+  );
 
-	// Get results
-	const results = $("body > main > div.row > div.col-lg-7").children(".card.mb-3");
+  // Get final listing
+  const final: NameMCUser[] = [];
 
-	// Get final listing
-	const final: NameMCUser[] = [];
+  return new Promise((resolve) => {
+    // Iterate through each result
+    results.each(async function (this: cheerio.Root) {
+      // Get uuid of result
+      const data = $(this);
+      const uuid = data
+        .children(".card-header.py-0")
+        .children("a")
+        .children(".row")
+        .children(".col")
+        .children("samp")
+        .text();
 
-	return new Promise(resolve => {
+      // Push full lookup to results
+      final.push(await fetch(uuid));
 
-		// Iterate through each result
-		results.each(async function(this: cheerio.Root) {
-
-			// Get uuid of result
-			const data = $(this);
-			const uuid = data.children(".card-header.py-0").children("a").children(".row").children(".col").children("samp").text();
-
-			// Push full lookup to results
-			final.push(await fetch(uuid));
-
-			// Resolve when al have been finished
-			if (results.length === final.length) return resolve(final);
-
-		});
-
-	}) as Promise<Array<NameMCUser>>;
-	// */
-
+      // Resolve when al have been finished
+      if (results.length === final.length) return resolve(final);
+    });
+  }) as Promise<Array<NameMCUser>>;
+  // */
 }
